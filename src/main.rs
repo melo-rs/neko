dd77b9e0b8aafacdab6deca9a3692503186208b9
Davi Melo <davimelo01082008@gmail.com>
2026-08-27 08:56:45 -0300

    refactor: refactor implementation

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 
 use core::{
     arch::{asm, global_asm},
-    panic::PanicInfo,
     mem::MaybeUninit,
+    panic::PanicInfo,
 };
-use neko::x86_64::SYS_EXIT;
+use neko::x86_64::{AT_FDCWD, STDOUT_FILENO, SYS_EXIT, SYS_OPENAT, SYS_READ, SYS_WRITE};
 
 #[panic_handler]
 fn on_panic(_info: &PanicInfo) -> ! {
@@ -26,57 +26,38 @@ _start:
 pub extern "C" fn do_start(rsp_ptr: *const usize) -> ! {
     unsafe {
         let pathname_ptr = *rsp_ptr.add(2);
-        let mut file_descriptor = 0isize;
 
-        asm!(
-            "syscall",
-            in("rax") 257,
-            in("rdi") -100,
-            in("rsi") pathname_ptr,
-            in("rdx") 0,
-            in("r10") 0,
-            lateout("rax") file_descriptor,
-            lateout("rcx") _,
-            lateout("r11") _,
-        );
+        let file_descriptor = openat(AT_FDCWD, pathname_ptr as *const u8, 0, 0);
 
         let mut buffer = MaybeUninit::<[u8; 4096]>::uninit();
 
         loop {
-            let mut read = 0usize;
+            let _read = read(file_descriptor, buffer.as_mut_ptr() as *mut u8, 4096);
 
-            asm!(
-                "syscall",
-                in("rax") 0,
-                in("rdi") file_descriptor,
-                in("rsi") buffer.as_mut_ptr() as *mut u8,
-                in("rdx") 4096,
-                lateout("rax") read,
-                lateout("rcx") _,
-                lateout("r11") _
-            );
-
-            if read == 0 {
-                break
+            if _read < 0 {
+                exit(1);
             }
+
+            if _read == 0 {
+                break;
+            }
+
+            let _read = _read as usize;
 
             let mut offset = 0usize;
 
-            while offset < read {
-                let mut written = 0usize;
-
-                asm!(
-                    "syscall",
-                    in("rax") 1,
-                    in("rdi") 1,
-                    in("rsi") (buffer.as_ptr() as *const u8).add(offset),
-                    in("rdx") read - offset,
-                    lateout("rax") written,
-                    lateout("rcx") _,
-                    lateout("r11") _
+            while offset < _read {
+                let written = write(
+                    STDOUT_FILENO,
+                    (buffer.as_ptr() as *const u8).add(offset),
+                    _read - offset,
                 );
 
-                offset += written
+                if written <= 0 {
+                    exit(1);
+                }
+
+                offset += written as usize
             }
         }
 
@@ -84,8 +65,8 @@ pub extern "C" fn do_start(rsp_ptr: *const usize) -> ! {
     }
 }
 
-/// Terminates the process with the given exit code.
-/// 
+/// Terminates the current process with the given exit code.
+///
 /// This implementation is specific to Linux on x86-64.
 fn exit(code: usize) -> ! {
     unsafe {
@@ -96,4 +77,65 @@ fn exit(code: usize) -> ! {
             options(noreturn)
         )
     }
+}
+
+// TODO: switch to Result<i32, Errno>
+fn openat(dirfd: i32, pathname: *const u8, flags: i32, mode: u32) -> i32 {
+    let file_descriptor: i32;
+
+    unsafe {
+        asm!(
+            "syscall",
+            in("rax") SYS_OPENAT,
+            in("rdi") dirfd,
+            in("rsi") pathname,
+            in("rdx") flags,
+            in("r10") mode,
+            lateout("rax") file_descriptor,
+            lateout("rcx") _,
+            lateout("r11") _,
+        )
+    };
+
+    return file_descriptor;
+}
+
+// TODO: switch to Result<isize, Errno>
+fn read(fd: i32, buf: *mut u8, count: usize) -> isize {
+    let result: isize;
+
+    unsafe {
+        asm!(
+            "syscall",
+            in("rax") SYS_READ,
+            in("rdi") fd,
+            in("rsi") buf,
+            in("rdx") count,
+            lateout("rax") result,
+            lateout("rcx") _,
+            lateout("r11") _,
+        )
+    }
+
+    return result;
+}
+
+// TODO: switch to Result<isize, Errno>
+fn write(fd: i32, buf: *const u8, count: usize) -> isize {
+    let result: isize;
+
+    unsafe {
+        asm!(
+            "syscall",
+            in("rax") SYS_WRITE,
+            in("rdi") fd,
+            in("rsi") buf,
+            in("rdx") count,
+            lateout("rax") result,
+            lateout("rcx") _,
+            lateout("r11") _,
+        )
+    }
+
+    return result;
 }
--- src/x86_64.rs
+++ src/x86_64.rs
@@ -1,4 +1,30 @@
+/// Special `dirfd` value that resolves relative paths against the
+/// current working directory.
+///
+/// See [`openat(2)`].
+///
+/// [`openat(2)`]: https://man7.org/linux/man-pages/man2/open.2.html
+pub const AT_FDCWD: i32 = -100;
+
+/// x86-64 Linux syscall number for ['openat(2)'].
+///
+/// ['openat(2)']: https://man7.org/linux/man-pages/man2/open.2.html
+pub const SYS_OPENAT: usize = 257;
+
 /// x86-64 Linux syscall number for [`exit(2)`].
-/// 
+///
 /// [`exit(2)`]: https://man7.org/linux/man-pages/man2/_exit.2.html
 pub const SYS_EXIT: usize = 60;
+
+/// x86-64 Linux syscall number for ['read(2)'].
+///
+/// ['read(2)']: https://man7.org/linux/man-pages/man2/read.2.html
+pub const SYS_READ: usize = 0;
+
+/// x86-64 Linux syscall number for ['write(2)'].
+///
+/// ['write(2)']: https://man7.org/linux/man-pages/man2/write.2.html
+pub const SYS_WRITE: usize = 1;
+
+/// File descriptor for the standard output stream.
+pub const STDOUT_FILENO: i32 = 1;
